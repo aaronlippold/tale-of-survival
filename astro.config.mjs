0b88de12eb5ca06c7eb504bd63e05a5a61d77bd9
Aaron Lippold <lippold@gmail.com>
2025-01-11 20:20:37 -0500

    reorganized the gallery into a component and fixed the partytown errors, disabled the lighthouse plugin
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,23 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tale-of-survival.netlify.app',
-  integrations: [tailwind(), sitemap(), partytown(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        debug: true,
+        resolveUrl: (url) => {
+          const pathname = new URL(url).pathname;
+          if (pathname.includes('/~partytown/')) {
+            return url.replace('/~partytown/', '/_astro/partytown/');
+          }
+          return url;
+        },
+      },
+    }),
+    robotsTxt()
+  ],
   output: 'server',
   adapter: netlify(), // Changed to Netlify adapter
 });
