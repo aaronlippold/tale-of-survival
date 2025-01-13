1b28cf2ebe9d035da77daf534cc3b5ff0c369ee4
Aaron Lippold <lippold@gmail.com>
2025-01-13 03:10:53 -0500

    hopefully it all works now
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 import robotsTxt from 'astro-robots-txt';
+import icon from "astro-icon";  // Add this import
 
 export default defineConfig({
   site: 'https://tale-of-survival.netlify.app',
   integrations: [
     tailwind(),
     sitemap(),
-    robotsTxt()
+    robotsTxt(),
+    icon({
+      include: {
+        heroicons: ['chevron-left-20-solid', 'chevron-right-20-solid']
+      }
+    })
   ],
   output: 'server',
   adapter: netlify(),
